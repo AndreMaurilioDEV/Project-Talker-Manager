914a975a5d4489a004d86b56f3f3823f9317d77c
André Maurilio <andreandrade1920@gmail.com>
2024-03-08 21:37:59 -0300

    lint

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 
-const {getData, getTalkerByID, generateTokenLogin} = require('./utils/fsUtils');
+const { getData, getTalkerByID, generateTokenLogin } = require('./utils/fsUtils');
 
 // não remova esse endpoint, e para o avaliador funcionar
 
@@ -15,14 +15,22 @@ const isValidEmail = (email) => {
   return emailRegex.test(email);
 };
 
-const validationTalker = (req, res, next) => {
-  const {email, password} = req.body;
+const validationTalker = (req, res) => {
+  const { email, password } = req.body;
   const emailValid = isValidEmail(email);
-  if (!email) { return res.status(400).json({message: 'O campo \"email\" é obrigatório' })}
-  if (!emailValid) {return res.status(400).json({message: 'O \"email\" deve ter o formato \"email@email.com\"' })}
-  if (!password) { return res.status(400).json({message: 'O campo \"password\" é obrigatório' })}
-  if (password.length <  6) { return res.status(400).json({message: 'O \"password\" deve ter pelo menos 6 caracteres' })}
-}
+  if (!email) { return res.status(400).json({ message: 'O campo "email" é obrigatório' }); }
+  if (!emailValid) {
+    return res.status(400).json(
+      { message: 'O "email" deve ter o formato "email@email.com"' },
+    );
+  }
+  if (!password) { return res.status(400).json({ message: 'O campo "password" é obrigatório' }); }
+  if (password.length < 6) {
+    return res.status(400).json(
+      { message: 'O "password" deve ter pelo menos 6 caracteres' },
+    );
+  }
+};
 
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -34,21 +42,20 @@ app.listen(PORT, () => {
 
 app.get('/talker', async (req, res) => {
   const talkers = await getData();
-  res.status(200).json(talkers)
+  res.status(200).json(talkers);
 });
 
 app.get('/talker/:id', async (req, res) => {
-  const {id} = req.params;
+  const { id } = req.params;
   const talkersPerID = await getTalkerByID(Number(id));
   if (talkersPerID) {
-    return res.status(200).json(talkersPerID)
+    return res.status(200).json(talkersPerID);
   }
-    else {
-    } res.status(404).json({message: "Pessoa palestrante não encontrada"});
-      
+    
+  res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
 app.post('/login', validationTalker, async (req, res) => {
   const randomToken = generateTokenLogin();
-  res.status(200).json({token: randomToken})
+  res.status(200).json({ token: randomToken });
 });
--- src/utils/fsUtils.js
+++ src/utils/fsUtils.js
@@ -3,37 +3,31 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const getData = async () => {
-    const path_data = '../talker.json';
-    try {
-        const data = await fs.readFile(path.join(__dirname, path_data), 'utf-8')
-        return JSON.parse(data);
-    } catch (error) {
-        return [];
-    }
+  const pathData = '../talker.json';
+  try {
+    const data = await fs.readFile(path.join(__dirname, pathData), 'utf-8');
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
 };
 
-
 const getTalkerByID = async (id) => {
-    const data = await getData();
-    return data.find((talker) => talker.id === id);
-}
+  const data = await getData();
+  return data.find((talker) => talker.id === id);
+};
 
 const generateRandomToken = () => {
   const tokenLength = 16;
-  return crypto.randomBytes(tokenLength/2).toString('hex');
+  return crypto.randomBytes(tokenLength / 2).toString('hex');
 };
 
-const generateTokenLogin = (bodyContent) => {
-    //const login = {... bodyContent};
-    return generateRandomToken();
-}
-
-
-
-
+const generateTokenLogin = () => 
+// const login = {... bodyContent};
+  generateRandomToken();
 
 module.exports = {
-    getData,
-    getTalkerByID,
-    generateTokenLogin
-}
+  getData,
+  getTalkerByID,
+  generateTokenLogin,
+};
